cd9f6a2f069f3aba219f1a643b024a9c9d688838
Ohad Segal <segalohad@gmail.com>
2023-05-09 15:00:36 +0300

    Add login-page
    1.Add login, logout pathes to the meetballs/urls.py file
    2.Add login, logut methods to render relvant pages in the meet_balls_app/views.py file
    3.Add login.html template file
    4.Modify base.html file with the relvant new href's

--- meet_balls_app/views.py
+++ meet_balls_app/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 
 def home(request):
+    return render(request, 'meet_balls_app/home.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            return render(request,'meet_balls_app/home.html', {'user': {'is_authenticated': True}})
+        else:   
+            error_message = "Invalid username or password."
+            messages.error(request, error_message)
+            return render(request, 'meet_balls_app/login.html')
+    else:
+        return render(request, 'meet_balls_app/login.html')
+
+def logout(request):
+    return render(request, 'meet_balls_app/home.html', {'user': {'is_authenticated': False}})
+
+def register(request):
     return render(request, 'meet_balls_app/home.html')
